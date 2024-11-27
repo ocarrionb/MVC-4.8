9183b2761af7cec15c399566ebee4e0dd1e5e2f7
Octavio Carrion <octcarbar@gmail.com>
2024-11-26 18:57:39 -0600

    Add new controller and get info from DB

---
+++ Test/Controllers/DonutsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Test.Models.BD;
+
+namespace Test.Controllers
+{
+    public class DonutsController : Controller
+    {
+        //
+        // GET: /Donuts/
+
+        public ActionResult Index()
+        {
+            List<Donuts> donuts = new List<Donuts>();
+            using (var db = new Models.BD.GralContext())
+            {
+                donuts = (from d in db.Donuts
+                          select new Donuts
+                          {
+                              Id = d.Id,
+                              Name = d.Name
+                          }).ToList();
+            }
+
+            return View(donuts);
+        }
+
+    }
+}
